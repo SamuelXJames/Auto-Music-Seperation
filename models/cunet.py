dcf91ecf7f9ad83a91089119f852cafe3708b121
SamuelXJames <31450279+SamuelXJames@users.noreply.github.com>
2021-04-16 17:01:45 -0400

    Update cunet.py

--- models/cunet.py
+++ models/cunet.py
@@ -57,7 +57,7 @@ def cnn_control(n_conditions, n_filters):
         - n_conditions = 1008
         - n_filters = [16, 32, 64]
     """
-    input_conditions = Input(shape=(config.Z_DIM, 1), name = 'Film Input')
+    input_conditions = Input(shape=(config.Z_DIM, 1))
     initializer = tf.random_normal_initializer(stddev=0.02)
     cnn = cnn_block(
         input_conditions, n_filters, config.Z_DIM, config.PADDING, initializer
